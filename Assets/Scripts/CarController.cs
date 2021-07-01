a3901d91484e800f582f3619bf6c8a84885d92a7
Valentin <valio31032@gmail.com>
2021-07-01 13:22:07 +0300

    Made the car settings modular
    Basically converted to ScriptableObject

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -4,56 +4,54 @@ using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
-	[SerializeField] private float airDrag, groundDrag;
+	[SerializeField] private CarSettings carSettings;
 	[SerializeField] private LayerMask groundLayer;
 	[SerializeField] private bool isGrounded;
 	[SerializeField] private Rigidbody sphereRB;
 	[SerializeField] private float moveInput, turnInput;
-	[SerializeField] private float fwdSpeed, reverseSpeed, turnSpeed;
-	void Start()
-	{
+	[SerializeField] private float distanceFromGround = 0.5f;
+	[SerializeField] private Transform groundRay;
 
+	private void Start()
+	{
+		sphereRB.mass = carSettings.Weight;
 	}
 	//TODO disable controls while in air
-	//TODO kill myself
 
 
 	// Update is called once per frame
-	void Update()
+	private void Update()
 	{
 		moveInput = Input.GetAxisRaw("Vertical");
 		turnInput = Input.GetAxisRaw("Horizontal");
 
-		moveInput *= moveInput > 0 ? fwdSpeed : reverseSpeed;
-
-		transform.position = sphereRB.transform.position;
-
-		turnInput *= turnSpeed * Time.deltaTime * Input.GetAxisRaw("Vertical"); //TODO multiply with normalized acceleration from sphereRB?
-		transform.Rotate(0, turnInput, 0, Space.World);
-
-		RaycastHit hit;
-		isGrounded = Physics.Raycast(transform.position, -transform.up, out hit, groundLayer);
-		transform.rotation = Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation;
+		moveInput *= moveInput > 0 ? carSettings.MaxSpeed : carSettings.MaxReverseSpeed;
 
 		if (isGrounded)
 		{
-			sphereRB.drag = groundDrag;
+			turnInput *= carSettings.TurnSpeed * Time.deltaTime * Input.GetAxisRaw("Vertical"); //TODO multiply with normalized acceleration from sphereRB?
+			transform.Rotate(0, turnInput, 0, Space.World);
 		}
-		else
-		{
-			sphereRB.drag = airDrag;
-		}
+
+		transform.position = sphereRB.transform.position;
 	}
 
 	private void FixedUpdate()
 	{
+		isGrounded = false;
+		RaycastHit hit;
+		if (Physics.Raycast(groundRay.position, -transform.up, out hit, groundLayer))
+		{
+			isGrounded = true;
+		}
+
 		if (isGrounded)
 		{
 			sphereRB.AddForce(transform.forward * moveInput, ForceMode.Acceleration);
 		}
 		else
 		{
-			sphereRB.AddTorque(transform.up * Physics.gravity.y);
+			sphereRB.AddForce(Vector3.up * -carSettings.Weight * 100);
 
 		}
 	}
---
+++ Assets/Scripts/Scriptable Objects/CarSettings.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+[CreateAssetMenu(fileName = "CarSettings", menuName = "Scriptable Objects/CarSettings", order = 0)]
+public class CarSettings : ScriptableObject
+{
+	public float MaxSpeed, MaxReverseSpeed, TurnSpeed;
+	public float AirDrag, GroundDrag;
+	public float Weight;
+}
